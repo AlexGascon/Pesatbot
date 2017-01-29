9164c0b7c272da2522771e5650621ff192c2e9b5
Alex Gascon <alexgascon.93@gmail.com>
2017-01-30 00:33:57 +0100

    First commit

---
+++ pesatbot.py
@@ -0,0 +1,14 @@
+import telebot
+
+TOKEN = "135590410:AAHt91I6GrFBQv9QTxEsU6YeA0Aj1qfOBKE"
+pesatbot = telebot.TeleBot(TOKEN)
+
+pesatbot.polling(none_stop=True, interval=0, block=True)
+
+pesatbot.set_webhook('http://pesatbot.herokuapp.com')
+
+
+@bot.message.handler(func=lambda m: True)
+def pole_reply(message):
+	if message.text.lower().contains('pole'):
+		pesatbot.reply_to(message, 'Tio, eres un puto pesat')
