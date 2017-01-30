a59c3fc903df39cac5dd1c70cdfc7ad4b96f922c
Alex Gascon <alexgascon.93@gmail.com>
2017-01-30 01:10:11 +0100

    Heroku bug fixing

--- pesatbot.py
+++ pesatbot.py
@@ -20,7 +20,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://pesatbot.herokuapps.com/bot")
+    bot.set_webhook(url="https://pesatbot.herokuapp.com/bot")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
