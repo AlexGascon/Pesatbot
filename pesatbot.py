f214e6a56e3f24f30769908485e225aab5f6656f
Alex Gascon <alexgascon.93@gmail.com>
2017-01-30 01:03:29 +0100

    Bug fixing

--- pesatbot.py
+++ pesatbot.py
@@ -3,9 +3,9 @@ import telebot
 TOKEN = "135590410:AAHt91I6GrFBQv9QTxEsU6YeA0Aj1qfOBKE"
 pesatbot = telebot.TeleBot(TOKEN)
 
-@pesatbot.message_handler(func=lambda m: True)
+@pesatbot.message_handler(func=lambda message: True)
 def pole_reply(message):
-	if message.text.lower().contains('pole')
+	if 'pole' in message.text.lower():
 		pesatbot.reply_to(message, 'Tio, eres un puto pesat')
 
 pesatbot.polling(none_stop=True, interval=0)
