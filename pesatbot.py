6522ac5c9699345aa7ca0777cbd3135385928327
Alex Gascon <alexgascon.93@gmail.com>
2017-01-30 01:08:52 +0100

    Trying to make the bot work in Heroku

--- pesatbot.py
+++ pesatbot.py
@@ -1,13 +1,27 @@
 import telebot
+from flask import Flask, request
 
 TOKEN = "135590410:AAHt91I6GrFBQv9QTxEsU6YeA0Aj1qfOBKE"
 pesatbot = telebot.TeleBot(TOKEN)
+
+server = Flask(__name__)
 
 @pesatbot.message_handler(func=lambda message: True)
 def pole_reply(message):
 	if 'pole' in message.text.lower():
 		pesatbot.reply_to(message, 'Tio, eres un puto pesat')
 
-pesatbot.polling(none_stop=True, interval=0)
 
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://pesatbot.herokuapps.com/bot")
+    return "!", 200
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+server = Flask(__name__)
