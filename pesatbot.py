a89d2a2cb243d91235a83016ea2bfd6c7032c0ad
Alex Gascon <alexgascon.93@gmail.com>
2017-01-30 16:28:23 +0100

    Bug fixing

--- pesatbot.py
+++ pesatbot.py
@@ -1,4 +1,5 @@
 import telebot
+import os
 
 TOKEN = "135590410:AAHt91I6GrFBQv9QTxEsU6YeA0Aj1qfOBKE"
 pesatbot = telebot.TeleBot(TOKEN)
@@ -7,6 +8,11 @@ pesatbot = telebot.TeleBot(TOKEN)
 @pesatbot.message_handler(func=lambda message: True)
 def pole_reply(message):
 	if 'pole' in message.text.lower():
+		print message.text.lower()
 		pesatbot.reply_to(message, 'Tio, eres un puto pesat')
 
-pesatbot.polling(none_stop=True)
+updater = Updater(TOKEN)
+updater.start_webhook(listen="0.0.0.0", port=5000, url_path=TOKEN)
+
+updater.bot.set_webhook("https://pesatbot.herokuapp.com/" + TOKEN)
+updater.idle()
